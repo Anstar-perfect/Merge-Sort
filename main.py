73fcc4972311deaff70fdb3a75bc456260bfe8dc
Anstar-perfect <adityanayak1110@gmail.com>
2026-05-03 15:22:25 +0530

    initial_commit

---
+++ main.py
@@ -0,0 +1,34 @@
+def merge_sort(a):
+    if len(a)>1:
+        mid = len(a)//2
+        left = a[:mid]
+        right = a[mid:]
+
+        merge_sort(left)
+        merge_sort(right)
+
+        i=0
+        j=0
+        k=0
+
+        while i<len(left) and j<len(right):
+            if left[i]<= right[j]:
+                a[k] = left[i]
+                i+=1
+            else:
+                a[k] = right[j]
+                j+=1
+
+        while i<len(left):
+            a[k] = left[i]
+            i+=1
+            k+=1
+        while j <len(right):
+            a[k] = right[j]
+            j+=1
+            k+=1
+
+a=[23,1,3,34,20,21]
+print('Unsorted Array',a)
+merge_sort(a)
+print('Sorted Array {}'.format(a))                                
